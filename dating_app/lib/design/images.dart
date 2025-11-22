c3c73a2a931c46e4c3f37bd488cd2429bc81d7b1
DovganMariya <feya.forests.05@mail.ru>
2025-11-22 20:43:00 +0300

    added svg images

---
+++ dating_app/lib/design/images.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture penImage = SvgPicture.asset('assets/images/chat_pen.svg');
+
+final SvgPicture glassImage = SvgPicture.asset('assets/images/chat_glass.svg');
+
+final SvgPicture crownImage = SvgPicture.asset('assets/images/chat_crown.svg');
+
+final SvgPicture medalImage = SvgPicture.asset('assets/images/chat_medal.svg');
