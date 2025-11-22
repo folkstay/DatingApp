2232252d636985f39531d3fde7af61f886a83ffe
DovganMariya <feya.forests.05@mail.ru>
2025-11-22 21:48:04 +0300

    app bar

--- dating_app/lib/design/colors.dart
+++ dating_app/lib/design/colors.dart
@@ -7,3 +7,9 @@ const Color secondaryColor = Color(0xFFF3F2F9);
 const Color secondaryVariantColor = Color(0xFFDB2A6B);
 
 const Color surfaceColor = Color(0xFFFFFFFF);
+
+const Color textColor = Color(0xFF2C2C2E);
+
+const Color messageColor = Color(0xFF84878D);
+
+const Color onlineColor = Color(0xFF1AB832);
---
+++ dating_app/lib/pages/chat/chat_page.dart
@@ -0,0 +1,43 @@
+import 'package:dating_app/design/colors.dart';
+import 'package:dating_app/design/images.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: surfaceColor,
+        centerTitle: true,
+        leading: Container(
+          alignment: Alignment.center,
+          child: penImage,
+        ),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 15),
+              child: glassImage)
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0), // Высота полоски
+          child: Container(
+            color: secondaryColor, // Цвет полоски
+            height: 3.0, // Толщина полоски
+          ),
+        ),
+        title: const Text('Сообщения',
+            style: TextStyle(
+              color: textColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Nunito',
+            )),
+      ),
+      body: Container(
+        color: surfaceColor, //child: const VehicleList()
+      ),
+    );
+  }
+}
